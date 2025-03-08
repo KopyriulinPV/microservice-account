b967388fc65794bc8e432a7302b0ae64cfba5609
@petr_kopyriulin <kpv636363@mail.ru>
2025-03-08 16:43:26 +0400

    ))

--- src/main/java/com/example/microservice/repository/AccountSpecifications.java
+++ src/main/java/com/example/microservice/repository/AccountSpecifications.java
@@ -33,7 +33,7 @@ public class AccountSpecifications {
 
             if (ageTo != null) {
                 ZonedDateTime currentDate = ZonedDateTime.now();
-                ZonedDateTime toDate = currentDate.minusYears(ageTo).plusDays(2);
+                ZonedDateTime toDate = currentDate.minusYears(ageTo).plusDays(364);
                 predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("birthDate"), toDate));
             }
 
